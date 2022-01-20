bb8e8c96799ad49ea3e7930f5b552283a66b8e45
Maryna Saikun <marynasaikun@gmail.com>
2022-01-20 19:50:25 +0200

    My app v.2.0.0

--- src/app/unauthorized/login/Login.tsx
+++ src/app/unauthorized/login/Login.tsx
@@ -1,3 +1,5 @@
+import axios                     from "axios";
+import { useNavigate }           from "react-router-dom";
 import { Formik }                from "formik";
 import styled                    from "styled-components";
 import Card                      from "@material-ui/core/Card";
@@ -10,28 +12,43 @@ import {
   PageForm,
   Btn,
 }                                from "../../../shared/styles";
+import { ILoginForm }            from "../../../shared/interfaces";
 import { signInFormSchema }      from "./validation";
-import { signIn }                from "./queries";
 
-const LoginPageCard = styled(Card)`
-  && {
-    margin-top: 100px;
-    min-width: 600px;
-    min-height: 320px;
+const LoginPageCard = styled(Card)`&& {
+  margin-top: 100px;
+  min-width: 600px;
+  min-height: 320px;
 
-    ${({ theme: { breakpoints } }: any) => breakpoints.up("md")} {
-      max-width: 700px;
-      max-height: 400px;
-    }
-
-    ${({ theme: { breakpoints } }: any) => breakpoints.up("ld")} {
-      max-width: 800px;
-    }
+  ${({ theme: { breakpoints } }: any) => breakpoints.up("md")} {
+    max-width: 700px;
+    max-height: 400px;
   }
-`;
+
+  ${({ theme: { breakpoints } }: any) => breakpoints.up("ld")} {
+    max-width: 800px;
+  }
+}`;
 
 export const Login = () => {
-  // const navigate = useNavigate();
+  const navigate = useNavigate();
+
+  function signIn(data: ILoginForm): void {
+    const { email, password } = data;
+  
+    axios
+      .post("https://61e029ae0f3bdb0017934e25.mockapi.io/api/v1/login", {
+        email,
+        password,
+      })
+      .then((response) => {
+        console.log(response.data);
+        navigate('/contacts');
+      })
+      .catch((error) => {
+        console.error(error);
+      });
+  }
 
   return (
     <>
